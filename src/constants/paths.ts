423734fcbf45eae980524fab358fa1b10dbc5d8f
Nelson-Chinedu <egwuchinedu69@gmail.com>
2026-02-06 06:37:55 +0100

    ft/signup

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,5 +1,7 @@
 const signin = "signin";
+const signup = "signup";
 
 export const AUTHS_PATHS = {
   SIGNIN: signin,
+  SIGNUP: signup,
 };
--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,14 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { lazy } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { AUTHS_PATHS } from "./constants/paths";
 import "./index.css";
-import Signin from "./pages/signin";
+
+const Signin_page = lazy(() => import("~/pages/signin"));
+const Signup_page = lazy(() => import("~/pages/signup"));
 
 export default function Root() {
   return (
     <div id="nova-mfe-auth">
       <BrowserRouter>
         <Routes>
-          <Route path={AUTHS_PATHS.SIGNIN} element={<Signin />} />
+          <Route path={""} element={<Navigate to={AUTHS_PATHS.SIGNIN} replace />} />
+          <Route path={AUTHS_PATHS.SIGNIN} element={<Signin_page />} />
+          <Route path={AUTHS_PATHS.SIGNUP} element={<Signup_page />} />
         </Routes>
       </BrowserRouter>
     </div>
